4c13bff92213264554d1d3c1013aaaf102ae9a83
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2022-01-05 16:47:47 +0500

    added widgets

--- main.py
+++ main.py
@@ -14,10 +14,29 @@ class UI(QMainWindow):
         self.setWindowTitle("Translator App")
 
         # Define our widgets
+        self.t_button = self.findChild(QPushButton, "pushButton")
+        self.c_button = self.findChild(QPushButton, "pushButton_2")
+
+        self.combo_1 = self.findChild(QComboBox, "comboBox")
+        self.combo_2 = self.findChild(QComboBox, "comboBox_2")
+
+        self.text_1 = self.findChild(QTextEdit, "textEdit")
+        self.text_2 = self.findChild(QTextEdit, "textEdit_2")
+
+        # Click the buttons
+        self.t_button.clicked.connect(self.translate)
+        self.c_button.clicked.connect(self.clear)
+
+
 
         # Show The App
         self.show()
 
+    def clear(self):
+        pass
+
+    def translate(self):
+        pass
 
 # Initialize The App
 app = QApplication(sys.argv)
