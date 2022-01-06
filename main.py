ecd27fdd6792073dc8b45aa36611039be4c91c43
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2022-01-06 15:59:57 +0500

    added actions

--- main.py
+++ main.py
@@ -27,16 +27,50 @@ class UI(QMainWindow):
         self.t_button.clicked.connect(self.translate)
         self.c_button.clicked.connect(self.clear)
 
+        # Add languages to the combo boxes
+        self.languages = googletrans.LANGUAGES
+        # print(self.languages)
+
+        # Convert to list
+        self.language_list = list(self.languages.values())
+        # print(self.language_list)
+
+        # Add items to combo boxes
+        self.combo_1.addItems(self.language_list)
+        self.combo_2.addItems(self.language_list)
+
+        # set default combo box
+        self.combo_1.setCurrentText("english")
+        self.combo_2.setCurrentText("uzbek")
 
 
         # Show The App
         self.show()
 
+
     def clear(self):
-        pass
+        # Clear the text boxes
+        self.textEdit.setText("")
+        self.textEdit_2.setText("")
+
+        # Reset the combo boxes
 
     def translate(self):
-        pass
+        try:
+            # Get original language key
+            for key, value in self.languages.items():
+                if(value == self.combo_1.currentText()):
+                    from_language_key = key
+            # Get translated language key
+            for key, value in self.languages.items():
+                if(value == self.combo_2.currentText()):
+                    to_language_key = key
+
+            self.text_1.setText(from_language_key)
+            self.text_2.setText(to_language_key)
+
+        except Exception as e:
+            QMessageBox.about(self, 'Translator', str(e))
 
 # Initialize The App
 app = QApplication(sys.argv)
